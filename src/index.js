2d7853494d4fb91cdb12f9b729e7771dcc130a65
Nikita <nikita.rusilko98@gmail.com>
2020-05-05 00:38:48 +0300

    Added second version

--- src/index.js
+++ src/index.js
@@ -8,4 +8,12 @@ const createFilterFunc = (query) => "return " + parseQuery(query);
 
 const filtredPersons = persons.filter((person) => new Function("person", createFilterFunc(query))(person));
 
-console.log(filtredPersons);
+console.log("First method ", filtredPersons);
+
+const createFilterFunc_2 = (query) => {
+  return (person) => eval(query.children.some((child) => child.type === "rule") && parseQuery(query));
+};
+
+const filtredPersons_2 = persons.filter((person) => createFilterFunc_2(query)(person));
+
+console.log("Second method ", filtredPersons_2);
